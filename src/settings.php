2229bd6bdddf774dc86c4515ac84126d85809a1b
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-08 17:35:04 +0430

    supporting subdirectory

--- src/settings.php
+++ src/settings.php
@@ -3,6 +3,7 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'route_prefix' => '/wise', // supporting sub-directory
 
         // Monolog settings
         'logger' => [
