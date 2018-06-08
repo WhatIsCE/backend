84a8182556f0d728af46452614c5befa3d45c456
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-08 12:28:53 +0430

    better error handling

--- src/routes.php
+++ src/routes.php
@@ -5,13 +5,16 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+*/
+
+define('DEBUG',true);
 
 $app->get('/notes/all/limit/{num}', function() {
 
@@ -19,14 +22,22 @@ $app->get('/notes/all/limit/{num}', function() {
     $get_id = intval($request->getAttribute('num'));
     $querystring = "select * from notes order by `date` LIMIT $get_id";
 
-    $results = query($query, [
-        "id" => $get_id
-    ],true);
+    try {
+        $results = query($querystring,[],true);
 
-    if (count($results)>0) {
-        echo json_encode($results);
-    } else {
-        echo '{"status":"ERROR"}';
+        if (count($results)>0) {
+         echo json_encode($results);
+        } else {
+            echo '{"status":"not found"}';
+        }
+    }
+    catch(Exception $e) {
+        if (DEBUG) {
+            echo $e->getMessage();
+        } else {
+            echo '{"status":"error"}';
+        }
+
     }
 
 });
@@ -37,14 +48,19 @@ $app->post('/notes/new', function($request){
     $querystring = "INSERT INTO notes (`author`,`content`,`date`) VALUES (:author,:content,NOW())";
     $note_author = $request->getParsedBody()['note_author'];
     $note_content = $request->getParsedBody()['note_content'];
-    $result = query($querystring, [
-        'author' => $note_author,
-        'content' => $note_content
-    ]);
-    if ($result == "969") {
-        echo '{"status":"done"}';
-    } else {
-        echo '{"status":"'.$result.'"}';
+
+    try {
+        query($querystring, [
+            'author' => $note_author,
+            'content' => $note_content
+        ]);
+    }
+    catch(Exception $e) {
+        if (DEBUG) {
+            echo $e->getMessage();
+        } else {
+            echo '{"status":"error"}';
+        }
     }
 
 
@@ -56,14 +72,24 @@ $app->get('/notes/{note_id}', function($request){
     $get_id = $request->getAttribute('note_id');
     $querystring = "SELECT * from notes WHERE `id`= :id";
 
-    $results = query($query, [
-        "id" => $get_id
-    ],true);
+    try {
+        $results = query($querystring, [
+            "id" => $get_id
+        ],true);
 
-    if (count($results)>0) {
-        echo json_encode($results);
-    } else {
-        echo '{"status":"ERROR"}';
+        if (count($results)>0) {
+         echo json_encode($results);
+        } else {
+            echo '{"status":"Not Found!"}';
+        }
     }
+    catch(Exception $e) {
+        if (DEBUG) {
+            echo $e->getMessage();
+        } else {
+            echo '{"status":"error"}';
+        }
+    }
+
 
 });
