ff4318026fc7ff5bb3cd7ec27754867916d56a69
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-11 13:00:35 +0430

    fixed subdirectory naming bug

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,7 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-        'route_prefix' => '/wise', // supporting sub-directory
+        'route_prefix' => '/wice', // supporting sub-directory
 
         // Monolog settings
         'logger' => [
